237f3b3de41fd2316048fe51dfe4e1800445518b
Pat Wey <patrickwey@gmail.com>
2019-09-15 12:01:48 -0700

    refactor: Update ListBooks to functional component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,53 +1,49 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Bookshelf from './Bookshelf';
 
-class ListBooks extends Component {
-  static propTypes = {
-    books: PropTypes.arrayOf(PropTypes.object),
-    shelves: PropTypes.arrayOf(PropTypes.shape({
-      title: PropTypes.string,
-      value: PropTypes.string,
-    })).isRequired,
-    showSearchPage: PropTypes.func.isRequired,
-    title: PropTypes.string.isRequired,
-    updateShelf: PropTypes.func.isRequired,
-  };
+function ListBooks({
+  books,
+  shelves,
+  showSearchPage,
+  title,
+  updateShelf,
+}) {
+  const visibleShelves = shelves.filter(({ value }) => value !== 'none')
 
-  visibleShelves = this.props.shelves.filter(({ value }) => value !== 'none')
-
-  render() {
-    const {
-      books,
-      shelves,
-      showSearchPage,
-      title,
-      updateShelf,
-    } = this.props;
-
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>{title}</h1>
-        </div>
-        <div className="list-books-content">
-          { this.visibleShelves.map(({ title, value }) => (
-            <Bookshelf
-              books={books.filter(book => book.shelf === value)}
-              key={value}
-              shelves={shelves}
-              title={title}
-              updateShelf={updateShelf}
-            />
-          ))}
-        </div>
-        <div className="open-search">
-          {/* TODO: use React router */}
-          <button onClick={showSearchPage}>Add a book</button>
-        </div>
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>{title}</h1>
       </div>
-    );
-  }
+      <div className="list-books-content">
+        { visibleShelves.map(({ title, value }) => (
+          <Bookshelf
+            books={books.filter(book => book.shelf === value)}
+            key={value}
+            shelves={shelves}
+            title={title}
+            updateShelf={updateShelf}
+          />
+        ))}
+      </div>
+      <div className="open-search">
+        {/* TODO: use React router */}
+        <button onClick={showSearchPage}>Add a book</button>
+      </div>
+    </div>
+  );
 }
+
+ListBooks.propTypes = {
+  books: PropTypes.arrayOf(PropTypes.object),
+  shelves: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    value: PropTypes.string,
+  })).isRequired,
+  showSearchPage: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  updateShelf: PropTypes.func.isRequired,
+};
 
 export default ListBooks;
